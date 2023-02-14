565569bd96d208ff852624dde071ae8798c9cd17
Welves de Paula <welvesdepaula@gmail.com>
2023-02-14 20:23:27 -0300

    dev

--- robot.ino
+++ robot.ino
@@ -11,7 +11,8 @@
 
 const int buttons[] = {1, 2, 3, 4, 5, 6};
 
-void setup() {
+void setup()
+{
   // Inicializa a comunicação com o Motor Shield
   Wire.begin();
 
@@ -19,51 +20,69 @@ void setup() {
   pinMode(Motor1A, OUTPUT);
   pinMode(Motor1B, OUTPUT);
   pinMode(Motor2A, OUTPUT);
+
   pinMode(Motor2B, OUTPUT);
   pinMode(Motor3A, OUTPUT);
   pinMode(Motor3B, OUTPUT);
   pinMode(Motor4A, OUTPUT);
   pinMode(Motor4B, OUTPUT);
-  
+
   // Configura os pinos dos botões como entradas
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 6; i++)
+  {
     pinMode(buttons[i], INPUT_PULLUP);
   }
 }
 
-void loop() {
+void loop()
+{
   // Controla o motor 1 de acordo com os estados dos botões
-  if (digitalRead(buttons[0]) == LOW) {
+  if (digitalRead(buttons[0]) == LOW)
+  {
     digitalWrite(Motor1A, HIGH);
     digitalWrite(Motor1B, LOW);
-  } else if (digitalRead(buttons[1]) == LOW) {
+  }
+  else if (digitalRead(buttons[1]) == LOW)
+  {
     digitalWrite(Motor1A, LOW);
     digitalWrite(Motor1B, HIGH);
-  } else {
+  }
+  else
+  {
     digitalWrite(Motor1A, LOW);
     digitalWrite(Motor1B, LOW);
   }
 
   // Controla o motor 2 de acordo com os estados dos botões
-  if (digitalRead(buttons[2]) == LOW) {
+  if (digitalRead(buttons[2]) == LOW)
+  {
     digitalWrite(Motor2A, HIGH);
     digitalWrite(Motor2B, LOW);
-  } else if (digitalRead(buttons[3]) == LOW) {
+  }
+  else if (digitalRead(buttons[3]) == LOW)
+  {
     digitalWrite(Motor2A, LOW);
     digitalWrite(Motor2B, HIGH);
-  } else {
+  }
+  else
+  {
     digitalWrite(Motor2A, LOW);
     digitalWrite(Motor2B, LOW);
   }
 
   // Controla o motor 3 de acordo com os estados dos botões
-  if (digitalRead(buttons[4]) == LOW) {
+  if (digitalRead(buttons[4]) == LOW)
+  {
     digitalWrite(Motor3A, HIGH);
     digitalWrite(Motor3B, LOW);
-  } else if (digitalRead(buttons[5]) == LOW) {
+  }
+  else if (digitalRead(buttons[5]) == LOW)
+  {
     digitalWrite(Motor3A, LOW);
     digitalWrite(Motor3B, HIGH);
-  } else {
+  }
+  else
+  {
     digitalWrite(Motor3A, LOW);
     digitalWrite(Motor3B, LOW);
   }
